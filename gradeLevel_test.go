ece4743076a0f9fbca564efc8689121931e07d03
Nick Stott <nick@nickstott.com>
2013-02-22 22:33:25 -0500

    add a colemanLiau test, and add more tests to the sentence tests

--- gradeLevel_test.go
+++ gradeLevel_test.go
@@ -7,19 +7,34 @@ import (
 
 var floatErr = 0.0001
 
+type textFloat struct {
+	in string
+	want float64
+}
+
+var text = []string{
+	"Existing computer programs that measure readability are based largely upon subroutines which estimate " + 
+		"number of syllables, usually by counting vowels. The shortcoming in estimating syllables is that " +
+		"it necessitates keypunching the prose into the computer. There is no need to estimate syllables " +
+		"since word length in letters is a better predictor of readability than word length in syllables. " +
+		"Therefore, a new readability formula was computed that has for its predictors letters per 100 words " +
+		"and sentences per 100 words. Both predictors can be counted by an optical scanning device, and thus " +
+		"the formula makes it economically feasible for an organization such as the U.S. Office of Education to " +
+		"calibrate the readability of all textbooks for the public school system.",
+	"Flesch Grade Level Readability Formula improves upon the Flesch Reading Ease Readability Formula. " +
+		"Rudolph Flesch, an author, writing consultant, and the supporter of Plain English Movement, " +
+		"is the co-author of this formula along with John P. Kincaid. That’s why it is also called Flesch-Kincaid " +
+		"Grade Level Readability Test. Raised in Austria, Flesch studied law and earned a Ph.D. " +
+		"in English from the Columbia University. Flesch, through his writings and speeches, advocated a return to phonics. " +
+		"In his article, A New Readability Yardstick, published in the Journal of Applied Psychology in 1948, Flesch proposed " +
+		"the Reading Ease Readability Formula. ",
+}
+
+
 func Test_FleschReadingEase(t *testing.T) {
-	var d = []struct {
-		in   string
-		want float64
-	}{
-		{"I LOVE PIE", 90.990000},
-		{"Flesch Grade Level Readability Formula improves upon the Flesch Reading Ease Readability Formula. " +
-			"Rudolph Flesch, an author, writing consultant, and the supporter of Plain English Movement, " +
-			"is the co-author of this formula along with John P. Kincaid. That’s why it is also called Flesch-Kincaid " +
-			"Grade Level Readability Test. Raised in Austria, Flesch studied law and earned a Ph.D. " +
-			"in English from the Columbia University. Flesch, through his writings and speeches, advocated a return to phonics. " +
-			"In his article, A New Readability Yardstick, published in the Journal of Applied Psychology in 1948, Flesch proposed " +
-			"the Reading Ease Readability Formula. ", 43.329821},
+	var d = []textFloat{
+		{text[0], 27.696487},
+		{text[1], 39.185238},
 	}
 
 	for _, v := range d {
@@ -31,18 +46,9 @@ func Test_FleschReadingEase(t *testing.T) {
 }
 
 func Test_FleschKincaidGradeLevel(t *testing.T) {
-	var d = []struct {
-		in   string
-		want float64
-	}{
-		{"I LOVE PIE", 1.313333},
-		{"Flesch Grade Level Readability Formula improves upon the Flesch Reading Ease Readability Formula. " +
-			"Rudolph Flesch, an author, writing consultant, and the supporter of Plain English Movement, " +
-			"is the co-author of this formula along with John P. Kincaid. That’s why it is also called Flesch-Kincaid " +
-			"Grade Level Readability Test. Raised in Austria, Flesch studied law and earned a Ph.D. " +
-			"in English from the Columbia University. Flesch, through his writings and speeches, advocated a return to phonics. " +
-			"In his article, A New Readability Yardstick, published in the Journal of Applied Psychology in 1948, Flesch proposed " +
-			"the Reading Ease Readability Formula.", 10.258929},
+	var d = []textFloat{
+		{text[0], 15.308807},
+		{text[1], 11.851429},
 	}
 
 	for _, v := range d {
@@ -52,6 +58,21 @@ func Test_FleschKincaidGradeLevel(t *testing.T) {
 		}
 	}
 }
+
+func Test_ColemanLiauIndex(t *testing.T) {
+	var d = []textFloat{
+		{text[0], 14.233950},
+		{text[1], 14.067755},
+	}
+
+	for _, v := range d {
+		r := ColemanLiauIndex(v.in)
+		if !compareFloats(r, v.want) {
+			t.Error(fmt.Sprintf("ColemanLiauIndex(%s) != %f, got %f", v.in, v.want, r))
+		}
+	}
+}
+
 
 func compareFloats(a, b float64) bool {
 	return a > (1-floatErr)*b && a < (1+floatErr)*b
--- sentences_test.go
+++ sentences_test.go
@@ -5,29 +5,13 @@ import (
 	"testing"
 )
 
-func Test_CountSentences(t *testing.T) {
-	var d = []struct {
-		in   string
-		want int
-	}{
-		{"This. Is. Three.", 3},
-		{"Que Paso?", 1},
-		{"fancy pants", 1},
-		{"I LOVE PIE! DON'T YOU LOVE PIE?", 2},
-	}
-
-	for _, v := range d {
-		if CountSentences(v.in) != v.want {
-			t.Error(fmt.Sprintf("CountSentences(%s) != %d, got: %d", v.in, v.want, CountSentences(v.in)))
-		}
-	}
+type textInt struct {
+	in string
+	want int
 }
 
-func Test_CountSentences2(t *testing.T) {
-	var d = []struct {
-		in   string
-		want int
-	}{
+func Test_CountSentences(t *testing.T) {
+	var d = []textInt{
 		{"This. Is. Three.", 3},
 		{"Que Paso?", 1},
 		{"fancy pants", 1},
@@ -36,10 +20,25 @@ func Test_CountSentences2(t *testing.T) {
 		{"That’s why it is also called Flesch-Kincaid " +
 			"Grade Level Readability Test. Raised in Austria, Flesch studied law and earned a Ph.D. " +
 			"in English from the Columbia University.", 2},
+		{"Existing computer programs that measure readability are based largely upon subroutines which estimate " + 
+			"number of syllables, usually by counting vowels. The shortcoming in estimating syllables is that " +
+			"it necessitates keypunching the prose into the computer. There is no need to estimate syllables " +
+			"since word length in letters is a better predictor of readability than word length in syllables. " +
+			"Therefore, a new readability formula was computed that has for its predictors letters per 100 words " +
+			"and sentences per 100 words. Both predictors can be counted by an optical scanning device, and thus " +
+			"the formula makes it economically feasible for an organization such as the U.S. Office of Education to " +
+			"calibrate the readability of all textbooks for the public school system.", 5},
+		{"Flesch Grade Level Readability Formula improves upon the Flesch Reading Ease Readability Formula. " +
+			"Rudolph Flesch, an author, writing consultant, and the supporter of Plain English Movement, " +
+			"is the co-author of this formula along with John P. Kincaid. That’s why it is also called Flesch-Kincaid " +
+			"Grade Level Readability Test. Raised in Austria, Flesch studied law and earned a Ph.D. " +
+			"in English from the Columbia University. Flesch, through his writings and speeches, advocated a return to phonics. " +
+			"In his article, A New Readability Yardstick, published in the Journal of Applied Psychology in 1948, Flesch proposed " +
+			"the Reading Ease Readability Formula. ", 6},
 	}
 
 	for _, v := range d {
-		c := CountSentences2(v.in)
+		c := CountSentences(v.in)
 		if c != v.want {
 			t.Error(fmt.Sprintf("CountSentences(%s) != %d, got: %d", v.in, v.want, c))
 		}
