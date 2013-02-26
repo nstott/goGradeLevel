2d2d47ac881e823863dc1042511b0bab172e530d
Nick Stott <nick@nickstott.com>
2013-02-26 17:00:47 -0500

    refine the interface, add a struct that holds all the data

--- gradeLevel.go
+++ gradeLevel.go
@@ -5,31 +5,70 @@ import (
 	"unicode"
 )
 
+type Result struct {
+	readingEase float64
+	fleschKincaidGradeLevel float64
+	colemanLiauIndex float64
+}
+
+
+func Analyze(s string) *Result {
+
+	numWords, numSyllables, numSentences := phraseStats(s)
+	numLetters := countLetters(s)
+
+	return &Result{
+		readingEase: calcFleschReadingEase(numWords, numSyllables, numSentences),
+		fleschKincaidGradeLevel: calcFleschKincaidGradeLevel(numWords, numSyllables, numSentences),
+		colemanLiauIndex: calcColemanLiauIndex(numWords, numLetters, numSentences),
+	}
+}
+
+
 func FleschReadingEase(s string) float64 {
 	numWords, numSyllables, numSentences := phraseStats(s)
-	score := 206.835 - (1.015 * (numWords / numSentences)) - (84.6 * (numSyllables / numWords))
+	return calcFleschReadingEase(numWords, numSyllables, numSentences)
 
-	return score
 }
+
+func calcFleschReadingEase(numWords, numSyllables, numSentences float64) float64 {
+	return 206.835 - (1.015 * (numWords / numSentences)) - (84.6 * (numSyllables / numWords))
+}
+
 
 func FleschKincaidGradeLevel(s string) float64 {
 	numWords, numSyllables, numSentences := phraseStats(s)
+	return calcFleschKincaidGradeLevel(numWords, numSyllables, numSentences)
+}
+
+func calcFleschKincaidGradeLevel(numWords, numSyllables, numSentences float64) float64 {
 	return (0.39*(numWords/numSentences) + 11.8*(numSyllables/numWords)) - 15.59
 }
 
 func ColemanLiauIndex(s string) float64 {
 	numWords, _, numSentences := phraseStats(s)
+	numLetters := countLetters(s)
+	return calcColemanLiauIndex(numWords, numLetters, numSentences)}
+
+func calcColemanLiauIndex(numWords, numLetters, numSentences float64) float64 { 
+	let := 100 * (numLetters / numWords)
+	sen := 100 * (numSentences / numWords)
+
+	return (0.0588 * let) - (0.296 * sen) - 15.8
+}
+
+/*
+ * utility functions to count things in the text, 
+ * like num Sentences, letters, syllables, etc
+ */
+func countLetters(s string) float64 {
 	numLetters := 0.0
 	for _, v := range []rune(s) {
 		if unicode.IsLetter(v) {
 			numLetters += 1.0
 		}
 	}
-
-	let := 100 * (numLetters / numWords)
-	sen := 100 * (numSentences / numWords)
-
-	return (0.0588 * let) - (0.296 * sen) - 15.8
+	return numLetters	
 }
 
 func phraseStats(s string) (numWords, numSyllables, numSentences float64) {
